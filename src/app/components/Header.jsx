98b0da63b9028f980e3141b466b3da783a1f48b4
Dante Del Favero <dante.delfavero@gmail.com>
2018-01-20 15:58:48 +0100

    Stateless componet for header

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 
-export class Header extends React.Component {
-    render(){
+export const Header = (props) => {
         return (
             <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
                 <div className="container collapse navbar-collapse">
                     <div className="navbar-header">
                         <ul className="navbar-nav mr-auto">
-                            <li className="nav-item"><a className="nav-link" href="#">Home</a></li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Home</a>
+                            </li>
                         </ul>
                     </div>
                 </div>
             </nav>
         );
-        
-    }
-}
+};
